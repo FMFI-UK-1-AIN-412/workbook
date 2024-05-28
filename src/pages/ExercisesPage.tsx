8be5b64028a9a08df27457c4110b42c00665c26a
Matej Mok <matej.mok@gmail.com>
2024-05-28 16:48:30 +0200

    ExercisePage: handle logged out user

--- src/pages/ExercisesPage.tsx
+++ src/pages/ExercisesPage.tsx
@@ -1,16 +1,25 @@
 import { Card, Container } from "react-bootstrap";
 import { useListExercisesReposQuery } from "../api/githubApi/graphqlApi/baseApi";
 import FormattedTextRenderer from "../components/FormattedTextRenderer";
+import { useAppSelector } from "../app/hooks";
+import { authSelectors } from "../features/auth/authSlice";
+import LoginPage from "./LoginPage";
+import { useLocation } from "react-router-dom";
 
 export default function ExercisesPage() {
-
+  const user = useAppSelector(authSelectors.user);
+  const location = useLocation();
   const exercisesRepo = useListExercisesReposQuery({ expression: 'HEAD:workbook.md' })
   console.log(exercisesRepo)
 
   const repoViews = exercisesRepo.isSuccess
     ? exercisesRepo.data.viewer.repositories.nodes.filter(p => p.object !== null)
     : []
-  
+
+  if (!user) {
+    return <LoginPage msg="Pre pokračovanie sa musíte prihlásiť" readirectTo={location.pathname} />
+  }
+
   return (
     <Container>
       <h1 className="my-3">Workbooks</h1>
